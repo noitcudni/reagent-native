30fe6d5909e4c54f52cac87d1d1b8e0f3f204328
Lih Chen <lihster@gmail.com>
2015-11-08 15:26:16 -0800

    before giving jmdns a try again.

--- ReagentNative/android/app/src/main/java/com/reagentnative/MainActivity.java
+++ ReagentNative/android/app/src/main/java/com/reagentnative/MainActivity.java
@@ -1,7 +1,9 @@
 package com.reagentnative;
 
 import android.app.Activity;
+import android.net.nsd.NsdServiceInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 
 import com.facebook.react.LifecycleState;
@@ -9,61 +11,81 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactRootView;
 import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
 import com.facebook.react.shell.MainReactPackage;
-import com.facebook.soloader.SoLoader;
+//import com.facebook.soloader.SoLoader;
 
 public class MainActivity extends Activity implements DefaultHardwareBackBtnHandler {
+  public static final String TAG = "MainActivity";
 
-    private ReactInstanceManager mReactInstanceManager;
-    private ReactRootView mReactRootView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        mReactRootView = new ReactRootView(this);
+  private ReactInstanceManager mReactInstanceManager;
+  private ReactRootView mReactRootView;
 
-        mReactInstanceManager = ReactInstanceManager.builder()
-                .setApplication(getApplication())
-                .setBundleAssetName("index.android.bundle")
-                .setJSMainModuleName("index.android")
-                .addPackage(new MainReactPackage())
-                .setUseDeveloperSupport(BuildConfig.DEBUG)
-                .setInitialLifecycleState(LifecycleState.RESUMED)
-                .build();
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    mReactRootView = new ReactRootView(this);
 
-        mReactRootView.startReactApplication(mReactInstanceManager, "ReagentNative", null);
+    //Log.d(TAG, "BuildConfig.Debug: " + BuildConfig.DEBUG);
+    mReactInstanceManager = ReactInstanceManager.builder()
+      .setApplication(getApplication())
+      .setBundleAssetName("index.android.bundle")
+      .setJSMainModuleName("index.android")
+      .addPackage(new MainReactPackage())
+      .setUseDeveloperSupport(BuildConfig.DEBUG)
+      .setInitialLifecycleState(LifecycleState.RESUMED)
+      .build();
 
-        setContentView(mReactRootView);
+    mReactRootView.startReactApplication(mReactInstanceManager, "ReagentNative", null);
+
+    setContentView(mReactRootView);
+  }
+
+  @Override
+  public void onStart() {
+    super.onStart();
+    registerService();
+  }
+
+  private void registerService() {
+    // Create the NsdServiceInfo object, and populate it.
+    NsdServiceInfo serviceInfo  = new NsdServiceInfo();
+
+    // The name is subject to change based on conflicts
+    // with other services advertised on the same network.
+    serviceInfo.setServiceName("reagent_android_native");
+    serviceInfo.setServiceType("_http._tcp.");
+    serviceInfo.setPort(9091);
+  }
+
+  @Override
+  public boolean onKeyUp(int keyCode, KeyEvent event) {
+    if (keyCode == KeyEvent.KEYCODE_MENU && mReactInstanceManager != null) {
+      mReactInstanceManager.showDevOptionsDialog();
+      return true;
     }
+    return super.onKeyUp(keyCode, event);
+  }
 
-    @Override
-    public boolean onKeyUp(int keyCode, KeyEvent event) {
-        if (keyCode == KeyEvent.KEYCODE_MENU && mReactInstanceManager != null) {
-            mReactInstanceManager.showDevOptionsDialog();
-            return true;
-        }
-        return super.onKeyUp(keyCode, event);
+  @Override
+  public void invokeDefaultOnBackPressed() {
+    super.onBackPressed();
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+
+    if (mReactInstanceManager != null) {
+      mReactInstanceManager.onPause();
     }
+  }
 
-    @Override
-    public void invokeDefaultOnBackPressed() {
-      super.onBackPressed();
+  @Override
+  protected void onResume() {
+    super.onResume();
+
+    if (mReactInstanceManager != null) {
+      mReactInstanceManager.onResume(this);
     }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        if (mReactInstanceManager != null) {
-            mReactInstanceManager.onPause();
-        }
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        if (mReactInstanceManager != null) {
-            mReactInstanceManager.onResume(this);
-        }
-    }
+  }
 }
